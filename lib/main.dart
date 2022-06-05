e16f66830c508f96e8dc27c1c7e93a00f3ca1a07
inf-reiazhalim <reiazbubt@gmil.com>
2022-06-05 14:04:59 +0900

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,23 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Mr.Reiaz";
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+        appBar: AppBar(
+          title: Text(
+            "30 Days Flutter learning with MrReiaz",
           ),
         ),
+        body: Center(
+          child: Container(
+            child: Text(
+              'Welcome to $days flutter learn with $name',
+            ),
+          ),
+        ),
+        drawer: Drawer(),
       ),
     );
   }
